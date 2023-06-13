ba9e6739130ba65d73874998a2df7954b4552d9c
YT Choong <e2choong@yahoo.com>
2023-06-13 09:58:01 +0800

    add basename

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://itosan2.github.io/pagination",
   plugins: [react()],
-})
+});
